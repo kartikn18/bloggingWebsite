7a0cb99d3fd1fc78d766cd72f293ec6e6cb380a4
Kartikn18 <narangkartik5@gmail.com>
2026-05-06 20:55:09 +0530

    initialize ts

---
+++ src/schema/db.ts
@@ -0,0 +1,12 @@
+export type user = {
+    id:number,
+    username:string,
+    email:string,
+    password:string,
+    created_at:Date,
+    updated:Date
+}
+export type post = {
+    id:number,
+    
+}
